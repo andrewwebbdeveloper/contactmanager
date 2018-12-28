3b8e8dc8c31913fbb0fab6b7deb8c53b73de7e98
Andrew Webb <andrew@Andrews-MacBook-Pro.local>
2018-12-28 12:51:33 -0500

    Updates

---
+++ precache-manifest.e3993f66897b757e58817cad0b9f20fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "f00a7f3715fe3761d13a",
+    "url": "/contactmanager/static/js/main.f00a7f37.chunk.js"
+  },
+  {
+    "revision": "0190f7dab3e567ade71b",
+    "url": "/contactmanager/static/js/1.0190f7da.chunk.js"
+  },
+  {
+    "revision": "f00a7f3715fe3761d13a",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "0190f7dab3e567ade71b",
+    "url": "/contactmanager/static/css/1.ba621e86.chunk.css"
+  },
+  {
+    "revision": "c74ca6fa1f8a9e545ef2befb8af73a84",
+    "url": "/contactmanager/index.html"
+  }
+];
